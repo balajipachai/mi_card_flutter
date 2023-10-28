f55d0634841a33aa22bc026268c35ce609e851db
Balaji Shetty Pachai <balaji.pachai08@gmail.com>
2023-10-28 23:29:35 +0530

    completes the micard application

--- lib/main.dart
+++ lib/main.dart
@@ -13,19 +13,73 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Container(width: 100.0, color: Colors.red),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(height: 100.0, width: 100.0, color: Colors.yellow),
-                Container(height: 100.0, width: 100.0, color: Colors.green),
-              ],
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/profile.jpeg'),
             ),
-            Container(width: 100.0, color: Colors.blue),
+            Text(
+              'Balaji Pachai',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                color: Colors.white,
+                fontSize: 40.0,
+              ),
+            ),
+            Text(
+              'SOFTWARE ENGINEER',
+              style: TextStyle(
+                  fontFamily: 'Source Sans 3',
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.1),
+            ),
+            SizedBox(
+              child: Divider(
+                color: Colors.white,
+              ),
+              height: 20.0,
+              width: 150.0,
+            ),
+            Card(
+                color: Colors.white,
+                margin: EdgeInsets.all(10.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal.shade400,
+                    size: 25.0,
+                  ),
+                  title: Text(
+                    '+91 123 456 7890',
+                    style: TextStyle(
+                        color: Colors.teal.shade400,
+                        fontFamily: 'Source Sans 3',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25.0),
+                  ),
+                )),
+            Card(
+              margin: EdgeInsets.all(10.0),
+              child: ListTile(
+                  leading: Icon(
+                    Icons.mail,
+                    color: Colors.teal.shade400,
+                    size: 25.0,
+                  ),
+                  title: Text(
+                    'bspachai@gmail.com',
+                    style: TextStyle(
+                        color: Colors.teal.shade400,
+                        fontFamily: 'Source Sans 3',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25.0),
+                  )),
+              color: Colors.white,
+            )
           ],
         )),
       ),
