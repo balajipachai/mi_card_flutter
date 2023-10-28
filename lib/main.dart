51783dc56d673ceeb96e09d6fdf0bb2b82c22784
Balaji Shetty Pachai <balaji.pachai08@gmail.com>
2023-10-28 18:04:43 +0530

    feat: completes the layout challenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(width: 100.0, color: Colors.red),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(height: 100.0, width: 100.0, color: Colors.yellow),
+                Container(height: 100.0, width: 100.0, color: Colors.green),
+              ],
+            ),
+            Container(width: 100.0, color: Colors.blue),
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
